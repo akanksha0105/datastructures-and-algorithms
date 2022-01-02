f329df733412454a6e7008f0175f823849c98ed7
Akanksha <mittalakanksha0105@gmail.com>
2022-01-02 14:47:23 +0530

    Problem 3 : Find the Second Largest Element

--- arrays/secondLargest.js
+++ arrays/secondLargest.js
@@ -3,21 +3,19 @@
 
 const getSecondLargest = (arr) => {
 	const n = arr.length;
-
+	if (n < 2) return "Array should have at least two numbers";
 	let largest = -1,
 		secondLargest = -1;
 
 	for (let i = 0; i < n; i++) {
-	
 		if (arr[i] > largest) {
 			secondLargest = largest;
 			largest = arr[i];
-		}
-
-	
-		else if (arr[i] < largest && arr[i] > secondLargest) {
+		} else if (arr[i] < largest && arr[i] > secondLargest) {
 			secondLargest = arr[i];
 		}
 	}
 	return secondLargest;
-}
+};
+
+// console.log(getSecondLargest([0, 3, 5, 2, 7, 9]));
