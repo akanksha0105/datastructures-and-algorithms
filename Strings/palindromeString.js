6da49a8b07d964542d71cb70902b9ff0293ddcad
Akanksha <mittalakanksha0105@gmail.com>
2024-01-05 18:40:13 +0530

    is string palindrome

--- Strings/palindromeString.js
+++ Strings/palindromeString.js
@@ -7,3 +7,26 @@ const isPalindrome = (str) => {
      
      return cleanedStr === reversedStr ? 1 : 0;
 }
+
+
+//can use 2 Two Pointer Approach
+
+const validPalindrome = (s) => {
+    // Use regex to remove non-alphanumeric characters and convert to lowercase
+    let cleanedStr = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    // Initialize two pointers
+    let left = 0;
+    let right = cleanedStr.length - 1;
+    
+    // Use two-pointer approach to compare characters from both ends
+    while (left < right) {
+        if (cleanedStr[left] !== cleanedStr[right]) {
+            return false; // Not a palindrome
+        }
+        left++;
+        right--;
+    }
+    
+    return true; // Is a palindrome
+}
