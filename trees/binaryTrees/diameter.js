13786d5cbbeba6a76202a19c29325627092fe37e
Akanksha <mittalakanksha0105@gmail.com>
2025-09-18 23:35:15 +0530

    Diameter of binary tree

---
+++ trees/binaryTrees/diameter.js
@@ -0,0 +1,22 @@
+function height(root) {
+	if (root === null) {
+		return 0; // return 0 instead of -1
+	}
+
+	let lHeight = height(root.left);
+	let rHeight = height(root.right);
+
+	return Math.max(lHeight, rHeight) + 1;
+}
+
+var diameterOfBinaryTree = function (root) {
+	if (root == null) return 0;
+
+	let lheight = height(root.left);
+	let rheight = height(root.right);
+
+	let ldiameter = diameterOfBinaryTree(root.left);
+	let rdiameter = diameterOfBinaryTree(root.right);
+
+	return Math.max(lheight + rheight, Math.max(ldiameter, rdiameter));
+};
