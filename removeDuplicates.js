cc825aec7c3d1467d72f53daacbdbfdc70908464
Akanksha <mittalakanksha0105@gmail.com>
2025-11-01 12:09:08 +0530

    Problem 1 : Remove Duplicates From Sorted Array

---
+++ removeDuplicates.js
@@ -0,0 +1,17 @@
+// Problem : https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+
+const removeDuplicates = (nums) => {
+	let x = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] > nums[x]) {
+			x++;
+			nums[x] = nums[i];
+		}
+	}
+	return x + 1;
+};
+
+
+// T.C : O(n)
+// S.C : O(1)
