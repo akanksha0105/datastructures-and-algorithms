f291efbdff3399085e3d058fe7af96222de6eaed
Akanksha <mittalakanksha0105@gmail.com>
2026-01-01 22:13:52 +0530

    1. Remove Duplicates

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,6 +1,6 @@
 // Problem : https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
 
-
+//Hint : x is the last unique element's position
 const removeDuplicates = (nums) => {
 	let x = 0;
 	for (let i = 0; i < nums.length; i++) {
