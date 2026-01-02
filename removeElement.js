c2e2527cde8bd2cf25cbf427f88e7387fe3c1601
Akanksha <mittalakanksha0105@gmail.com>
2026-01-02 23:21:53 +0530

    2. Remove Element

--- removeElement.js
+++ removeElement.js
@@ -1,11 +1,12 @@
-const removeElement = (nums) => {
-	let x = -1;
+//Problem : https://leetcode.com/problems/remove-element/description/?envType=problem-list-v2&envId=ww4l9yzs
 
+const removeElement = (nums, val) => {
+	let x = 0;
 	for (let i = 0; i < nums.length; i++) {
-		if (nums[i] !== val) {
-			x++;
+		if (nums[i] != val) {
 			nums[x] = nums[i];
+			x++;
 		}
 	}
-	return x + 1;
+	return x;
 };
